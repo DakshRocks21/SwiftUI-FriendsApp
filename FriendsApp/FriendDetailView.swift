31977b2ea1d9938be6a29c97f4d0e1161d7128eb
DakshRocks21 <85815311+DakshRocks21@users.noreply.github.com>
2021-07-03 10:31:49 +0800

    Added Text and added also contains the file I forgot to commit in the last one =_=

--- FriendsApp/FriendDetailView.swift
+++ FriendsApp/FriendDetailView.swift
@@ -18,6 +18,24 @@ struct FriendDetailView: View {
                 .scaledToFill()
                 .frame(height: 300)
             
+            Image(friend.name)
+                .resizable()
+                .scaledToFill()
+                .frame(width: 250, height: 250)
+                .mask(Circle())
+                .overlay(
+                    Circle()
+                        .stroke(lineWidth: 8)
+                        .foregroundColor(.white)
+                )
+                .offset(x: 0, y: -250 / 2)
+                .shadow(radius: 6)
+                .padding(.bottom, -250 / 2)
+            
+            Text("\(Image(systemName: friend.icon)) \(friend.school)")
+                .font(.system(size: 24))
+                .padding()
+            
             Spacer()
         }
         .navigationTitle(friend.name)
